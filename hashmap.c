37f487f9190450b85d97cc8e8fad96d7c1f257f8
repl.it user <replituser@example.com>
2020-09-25 15:47:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -46,18 +46,14 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
     long posicion = hash(key, map->capacity);//Se obtiene la posicion (a)
-    
     while(map->buckets[posicion] != NULL){
       posicion ++; //Resolucion colisiones (b)
+      if(posicion == map->capacity){
+      posicion = 0;
+      }
     }
-
     //int capacidadmax = (0.7 * map->capacity);
-
-
-    
     map->buckets[posicion] = createPair(key, value); //Ingresa el par en la casilla (c)
-
-
 
     map->current = posicion; //Actualiza el current
     map->size++; //Aumenta size ya que se agrega un elemento
