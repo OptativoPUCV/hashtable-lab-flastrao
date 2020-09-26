ae5ac26f00d522737ced3000f1790cc5eb398cd2
repl.it user <replituser@example.com>
2020-09-26 00:09:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -109,7 +109,7 @@ void * firstMap(HashMap * map) {
 
 void * nextMap(HashMap * map) {
     int posSig = map->current+1;
-    while(map->buckets[posSig]->key == NULL){
+    while(map->buckets[posSig] == NULL){
       posSig++; //Si es que no existe un value se sigue iterando
     }
     map->current = posSig;
